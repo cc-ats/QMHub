fdc110178d1414b2f649e33df74867dd1d93a5bb
panxl <panxl@users.noreply.github.com>
2017-05-18 22:39:30 -0500

    Tweak __init__.py for qmtools

--- namd_qmmm/qmtools/__init__.py
+++ namd_qmmm/qmtools/__init__.py
@@ -4,11 +4,12 @@ from .orca import ORCA
 from .mopac import MOPAC
 from .psi4 import PSI4
 
-__all__ = ['QChem', 'DFTB', 'ORCA', 'MOPAC', 'PSI4']
+__all__ = ['choose_qmtool']
 
+QMTOOLS = ['QChem', 'DFTB', 'ORCA', 'MOPAC', 'PSI4']
 
 def choose_qmtool(qmSoftware):
-    for qmtool in __all__:
+    for qmtool in QMTOOLS:
         qmtool = globals()[qmtool]
         if qmtool.check_software(qmSoftware):
             return qmtool
